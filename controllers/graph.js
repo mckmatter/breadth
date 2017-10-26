0514bf0f14a9174094d0183510157b620656bc3b
Dylan McKenna <dylan@mckmatter.com>
2017-10-26 09:25:07 -0400

    Touched Views

--- controllers/graph.js
+++ controllers/graph.js
@@ -1,2 +1,14 @@
 /*
-When a request is made for 
+When a request is made for a graph,
+this is the route.
+*/
+
+//Require Ezpress
+var express = require('express')
+var router = express.Router()
+
+router.get('/:id', function(req, res) {
+	res.render('graph'+req.param.id, {})
+})
+
+module.exports = router
