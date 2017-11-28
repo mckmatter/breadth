7ac86fecfb158ba3455584363d0e737f6b216367
Dylan McKenna <dylan@mckmatter.com>
2017-11-28 11:38:07 -0500

    Bind the application to port 80

--- app.js
+++ app.js
@@ -12,20 +12,20 @@ Fall 2017
 */
 
 //Require Frameworks and expose Express
-var mustache = require('mustache-express')
 var express = require('express')
 var app = express()
+var morgan = require('morgan')
 
-//Set View Engine and pass templates path to mustache
-app.engine('mustache', mustache())
-app.set('view engine', 'mustache')
-app.set('views', __dirname + '/views')
+//Log the request to the console with Morgan
+app.use(morgan('tiny'))
+
+
 
 //Setup Route to public
 app.use(express.static('public'))
 
 //Start Server
-var port = 3000
+var port = 80
 app.listen(port, function() {
 	console.log('App listenting at: ' + port)
 })
